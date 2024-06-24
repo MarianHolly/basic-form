f3cc579e55866bfde2d186d2b4ad64d28884e93d
Stupel <hellomariandev@gmail.com>
2024-06-24 13:29:01 +0200

    define form

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { Form } from "@/components/ui/form";
+import { formSchema } from "./schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export default function Home() {
+  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {username: ""}
+  })
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h2 className="text-center text-2xl-font-semibold">React Hook Form</h2>
+      <h2 className="text-center text-2xl-font-semibold my-8 text-stone-900">React Hook Form</h2>
+    
     </main>
   );
 }
